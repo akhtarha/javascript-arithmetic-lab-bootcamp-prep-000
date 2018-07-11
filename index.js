189514b756a69fd29dc9e4b822f1723af4891905
has_is_ere@hotmail.com <has_is_ere@hotmail.com>
2018-07-11 16:28:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,23 @@ function multiply(a,b){
 function divide(a,b){
   return a/b
 }
+
+function add(a,b){
+  return a+b
+}
+
+function add(a,b){
+  return a+b
+}
+
+function add(a,b){
+  return a+b
+}
+
+function add(a,b){
+  return a+b
+}
+
+function add(a,b){
+  return a+b
+}
